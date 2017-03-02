ef325fc88ba523d971a031a5a0badbac341b0ee8
xiongzhaoyang <xiongzhaoyang@infinibase.cn>
2017-03-02 11:16:57 +0800

    增加添加、删除、批量删除功能

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-#下载项目
+###提醒列表demo
+前端使用angular+bootstrap,后端使用express+node,数据库使用mongodb
+***
+##下载项目
 ```
 git clone https://github.com/xzy0315/ZYTodoList.git
 ```
-#安装前后台依赖的库
+##安装前后台依赖的库
 ```
 npm install && bower install
 ```
-#启动项目
+##启动项目
+```
 npm start
+```
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,55 @@
 var express = require('express');
 var router = express.Router();
+var todoModel = require('../model/index');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {});
+});
+
+router.route('/todos')
+.get(function(req, res, next) {
+
+    todoModel.find({},function(err,todos){
+        if(err){
+            res.send({code:0,msg:'查询错误'});
+        }else{
+            res.send(todos);
+        }
+    });
+
+})
+.post(function(req, res, next) {
+    todoModel.create(req.body,function(err,todo){
+        if(err){
+            res.send({code:0,msg:'添加错误'});
+        }else{
+            res.send(todo);
+        }
+    });
+
+})
+
+router.route("/todos/delete/:_id").delete(function(req, res, next) {
+    todoModel.remove({_id:req.params._id},function(err,result){
+        if(err){
+            res.send({code:0,msg:'删除错误'});
+        }else{
+            res.send({code:1,msg:'删除成功'});
+        }
+    });
+
+});
+
+router.route("/todos/batchDelete").post(function(req, res, next) {
+    console.log(req.body);
+    todoModel.remove({_id:{$in:req.body}},function(err,result){
+        if(err){
+            res.send({code:0,msg:'批量删除错误'});
+        }else{
+            res.send({code:1,msg:'批量删除成功'});
+        }
+    });
+
 });
 
 module.exports = router;
